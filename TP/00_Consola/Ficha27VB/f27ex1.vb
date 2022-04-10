32cfade90a77e741c841b35735a8ed7bc07a8b17
PaiPedro <103385256+PaiPedro@users.noreply.github.com>
2022-04-10 21:00:04 +0100

    Add files via upload

---
+++ TP/00_Consola/Ficha27VB/f27ex1.vb
@@ -0,0 +1,18 @@
+﻿Module f27ex1
+    Sub Main(args As String())
+        Dim senha = "senha"
+        Dim insertpass As String
+        Do
+            insertpass = pedirString("Insira a sua palavra passe: ")
+            If insertpass = senha Then
+                Console.WriteLine("Utilizador Autorizado!!!")
+            Else
+                Console.WriteLine("Utilizador Bloqueado!!!")
+            End If
+            esperaAlt()
+        Loop Until insertpass = senha
+        esperaAlt()
+
+    End Sub
+
+End Module
